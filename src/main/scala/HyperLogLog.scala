e52f69b9a4d8831012e8c97e269a3c88fdf82343
Murilo Deboni <murilodeboni@Murilos-MacBook-Air.local>
2023-05-09 21:03:41 +0200

    Intermediate logic WIP

--- src/main/scala/HyperLogLog.scala
+++ src/main/scala/HyperLogLog.scala
@@ -1,6 +1,10 @@
-import com.twitter.algebird.HyperLogLog.int2Bytes
-
+import HyperLogLogIntermediateUtils.saveArrayInFolder
+import com.twitter.algebird.HyperLogLog.{fromBytes, int2Bytes, toBytes}
 import com.twitter.algebird._
+
+import java.io.File
+import java.time.LocalDate
+import scala.collection.immutable
 
 trait DisplayApproximate {
   def algName: String
@@ -11,26 +15,33 @@ trait DisplayApproximate {
   }
 }
 
-case class BruteForce(dataList: Vector[Vector[Int]]) extends DisplayApproximate {
+case class BruteForce(dataList: Map[LocalDate, Vector[Int]]) extends DisplayApproximate {
   override def algName: String = "Brute Force"
 
   override def aprox: Approximate[Long] = {
-    val distinct = dataList.reduce(_ ++ _).distinct.length.toLong
+    val distinct = dataList.values.reduce(_ ++ _).distinct.length.toLong
     Approximate(distinct, distinct, distinct, 1)
   }
 }
 
-case class HyperLogLogMonoidTest(bits: Int = 10, dataList: Vector[Vector[Int]]) extends DisplayApproximate {
+case class HyperLogLogMonoidTest(bits: Int = 10, dataList: Map[LocalDate, Vector[Int]]) extends DisplayApproximate {
   override def algName: String = "HyperLogLogMonoid"
   override def aprox: Approximate[Long] = {
     val hllMonoid = new HyperLogLogMonoid(bits = bits)
-    val hllsList = dataList.map (
-      data => data.map(hllMonoid.create(_))
-    )
-    val HLLs = hllsList.map(hllMonoid.sum(_))
+    val hllsList: Map[LocalDate, Vector[HLL]] = dataList.map {
+      case (date, data) => date -> data.map(hllMonoid.create(_))
+    }
+
+    val HLLs = hllsList.map{case (date, hllVector) => (date, hllMonoid.sum(hllVector))}
+
+    val intermediate = HLLs.map{case (date, hll) => (date, toBytes(hll))}
+
+    intermediate.foreach{case (date, hll) => saveArrayInFolder(date, hll)}
+
+    val fromIntermediateToHLL = intermediate.map(i => fromBytes(i._2))
 
     // TODO: how to store intermediate steps?
-    val combinedHLL = HLLs.reduce(_ + _)
+    val combinedHLL = fromIntermediateToHLL.reduce(_ + _)
     hllMonoid.sizeOf(combinedHLL)
   }
 
@@ -46,9 +57,41 @@ object HyperLogLog extends App {
     result
   }
 
+  val date1: LocalDate = LocalDate.of(2019, 1, 1)
+  val date2: LocalDate = LocalDate.of(2019, 1, 2)
+  val date3: LocalDate = LocalDate.of(2019, 1, 2)
+
   val sample = ReadFile()
-  val data = Vector(sample.users, sample.users2, sample.users3)
+
+  val data = Map(date1 -> sample.users, date2 -> sample.users2, date3 -> sample.users3)
 
   time { HyperLogLogMonoidTest(14, data).show }
-  time { BruteForce(data).show }
+//  time { BruteForce(data).show }
+}
+
+object HyperLogLogIntermediateUtils {
+
+  def printToFile(f: File)(op: java.io.PrintWriter => Unit) {
+    val p = new java.io.PrintWriter(f)
+    try {
+      op(p)
+    } finally {
+      p.close()
+    }
+  }
+  def saveArrayInFolder(date: LocalDate, byteArray: Array[Byte]): Unit = {
+    //save byteArray to disk in folder with name date
+    printToFile(new File(f"$date.txt")) { p =>
+      byteArray.foreach(p.println)
+    }
+  }
+
+  def fileExists(date: LocalDate): Boolean = new File(f"$date.txt").exists()
+  def readFileToArray(date: LocalDate): Array[Byte] = {
+    //read file from disk and return array
+    val source = scala.io.Source.fromFile(f"$date.txt")
+    val byteArray = source.getLines.map(_.toByte).toArray
+    source.close()
+    byteArray
+  }
 }
